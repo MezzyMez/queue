78b2de426b206496772b17eeecfb80e47be247a6
Mezzy Mez <mezzydj@gmail.com>
2020-11-25 10:33:04 -0800

    Added dequeue

--- queue.py
+++ queue.py
@@ -30,6 +30,20 @@ class Queue:
       self.size += 1
     else:
       print("Sorry, no more room!")
+
+ def dequeue(self):
+    if self.is_empty() is not True:
+      item_to_remove = self.head
+      print("Removing " + str(item_to_remove.get_value()) + " from the queue!")
+      if self.size == 1:
+        self.head = None
+        self.tail = None
+      else:
+        self.head = self.head.get_next_node()
+      self.size -= 1
+      return item_to_remove.get_value()
+    else:
+      print("This queue is totally empty!")
     
   def peek(self):
     if self.is_empty():
