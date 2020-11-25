f41b80b8829759f19af3553541e7a810d3c61c17
Mezzy Mez <mezzydj@gmail.com>
2020-11-25 10:16:57 -0800

    Added enqueue

--- queue.py
+++ queue.py
@@ -10,28 +10,44 @@ Created on Wed Nov 25 08:56:13 2020
 from node import Node
 
 class Queue:
-  # Add max_size and size properties within __init__():
-  def __init__(self, max_size=None, size=0):
+  def __init__(self, max_size=None):
     self.head = None
     self.tail = None
     self.max_size = max_size
-    self.size = size
+    self.size = 0
+    
+  # Add your enqueue method below:
+  def enqueue(self, value):
+    if self.has_space():
+      item_to_add = Node(value)
+      print("Adding " + str(item_to_add.get_value()) + " to the queue!")
+      if self.is_empty():
+        self.head = item_to_add
+        self.tail = item_to_add
+      else:
+        self.tail.set_next_node(item_to_add)
+        self.tail = item_to_add
+      self.size += 1
+    else:
+      print("Sorry, no more room!")
     
   def peek(self):
     if self.is_empty():
-      print ("Nothing to see here!")
+      print("Nothing to see here!")
     else:
       return self.head.get_value()
   
-  # Define get_size() and has_space() below:
   def get_size(self):
     return self.size
   
   def has_space(self):
-    if self.max_size:
-      return True if (self.max_size>self.get_size()) else False
-    else:
+    if self.max_size == None:
       return True
+    else:
+      return self.max_size > self.get_size()
     
   def is_empty(self):
     return self.size == 0
+
+q = Queue()
+q.enqueue("all the fluffy kitties")
